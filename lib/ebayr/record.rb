70c93c8178fd24e4c3183b158e29b6a921d0be47
Cichol <cichol@live.cn>
2015-09-22 16:16:23 +0800

    modify convert_key

--- lib/ebayr/record.rb
+++ lib/ebayr/record.rb
@@ -35,8 +35,8 @@ module Ebayr
     end
 
     def self.convert_key(k)
-      #k.to_s.underscore.gsub(/e_bay/, "ebay").to_sym
-      k.to_s.camelize.gsub(/Ebay/, "eBay").to_sym
+      k.to_s.underscore.gsub(/e_bay/, "ebay").to_sym
+      #k.to_s.camelize.gsub(/Ebay/, "eBay").to_sym
     end
 
     def convert_value(arg)
